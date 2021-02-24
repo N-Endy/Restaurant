c95b338ff522432d2db30cedddda505f6d3e6fdf
N-Endy <okafornel@gmail.com>
2021-02-24 17:11:37 +0100

    Started work on the location map. Made other changes that i can't recall because this took a stretch of days but i changed the border radius of all buttons

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-// Declare map variable
-let map;
-
-// Initialize and add map
+// Initialize and add the map
 function initMap() {
-    // Centered map on location
-  map = new google.maps.Map(document.querySelector("#map"), {
-    //   My location
-    center: { lat: 6.623550, lng: 3.500190 },
-    zoom: 16
-  });
-}
+    // The location of Uluru
+    const lagos = { lat: 6.623550, lng: 3.500190 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: lagos,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+
+
 
 // Smooth Scrolling
-$('.navbar a').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+// $('.navbar a').on('click', function(event) {
+//     if (this.hash !== '') {
+//         event.preventDefault();
 
-        const hash = this.hash;
+//         const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top -100
-            },
-            800
-        );
-    }
-});
+//         $('html, body').animate(
+//             {
+//                 scrollTop: $(hash).offset().top -100
+//             },
+//             800
+//         );
+//     }
+// });
+
+// Logo
+// let logo = document.querySelector('.logo-name');
+// let blackbuffet = logo.textContent;
+// console.log(blackbuffet);
+// let firstLetter = blackbuffet.split('');
+// firstLetter[0].style.color = 'white';
+
+// var changeColor = function (className, color) {
+//     var elems = document.getElementsByClassName (className);
+//     console.log(elems[0].textContent.split('')[0])
+
+//     // Change the text color
+//     elems[0].textContent.split('')[0].st = color;
+// }
+// changeColor ("logo-name", "red");
