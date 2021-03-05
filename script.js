f9c5f05c73d3a490663523756eed36222d1731b6
N-Endy <okafornel@gmail.com>
2021-03-05 12:12:10 +0100

    Made the reservation section mobile responsive. Removed images and made button center

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
+    // The location of Lagos
     const lagos = { lat: 6.623550, lng: 3.500190 };
-    // The map, centered at Uluru
+    // The map, centered at Lagos
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 16,
       center: lagos,
@@ -15,19 +15,18 @@ function initMap() {
   }
 
 
+function hamburger() {
+  let ul = document.getElementById('ul');
+  let logo = document.getElementById('logo')
+  if (ul.style.display === 'none') {
+    ul.style.display = 'block';
+  } else {
+    ul.style.display = 'none';
+  }
 
-//  Smooth Scrolling
-$('.navbar a').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
-
-        const hash = this.hash;
-
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top -100
-            },
-            800
-        );
-    }
-});
+  if(ul.style.display === 'block') {
+    logo.style.display = 'none';
+  } else {
+    logo.style.display = 'flex';
+  }
+}
